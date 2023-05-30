bab0fb145e09c678bef9441418ddbdd7a3eacd32
Anuraag Gupta <anuraagg.kval3@gmail.com>
2023-05-30 16:03:24 +0530

    chore: removing console statement and updating setTimeout

--- scripts/Youtube Comments And Secondary Video Recommender Element Switcher.js
+++ scripts/Youtube Comments And Secondary Video Recommender Element Switcher.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube Comments And Secondary Video Recommender Element Switcher
 // @namespace    http://your-namespace.com
-// @version      1.0.3
+// @version      1.0.5
 // @description  Switches the positions of two elements on a web page
 // @match        *://*.youtube.com/*
 // @grant        none
@@ -11,6 +11,7 @@
 
 (function() {
     'use strict';
+    let checkElementsInterval;
     function switchElements(element1, element2) {
         // Get the parent node of both elements
         var parent1 = element1.parentNode;
@@ -23,11 +24,11 @@
         // Swap the positions of the elements by inserting them in their new positions
         parent1.insertBefore(element2, next1);
         parent2.insertBefore(element1, next2);
+        clearInterval(checkElementsInterval);
     }
     // Wait for the document to be fully loaded
     window.addEventListener('load', function() {
-        console.log("testing anuraag's script");
-        setTimeout(function() {
+        checkElementsInterval = setInterval(function() {
             var comments = document.getElementById('comments'); // Replace 'element1' with the ID of your first element
             var secondary = document.getElementById('secondary'); // Replace 'element2' with the ID of your second element
 
@@ -36,10 +37,7 @@
                 secondary.style = "width:100%";
                 // Youtube uses hardcoded px for video tags. This seemed easier for Mac's screeen
                 comments.style = "width:426px; height: 100vh;overflow: auto;";
-
-            } else {
-                console.error('One or both of the elements could not be found.');
             }
-        }, 1000); // Adjust the delay (in milliseconds) as needed
+        }, 500); // Adjust the delay (in milliseconds) as needed
     })
 })();
